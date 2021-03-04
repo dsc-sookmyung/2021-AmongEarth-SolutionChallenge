a6d31e73ea48d4911c8d1a0a3e7ea268b7c4199d
JIAH LEE <66239292+JIAH-LEA@users.noreply.github.com>
2021-03-05 00:46:47 +0900

    03.05 BadgeActivity Load Firebase Data

--- AmongEarth/app/src/main/java/com/example/amongearth/mypage/BadgeAdapter.java
+++ AmongEarth/app/src/main/java/com/example/amongearth/mypage/BadgeAdapter.java
@@ -1,58 +1,71 @@
+
+
 package com.example.amongearth.mypage;
 
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 import com.example.amongearth.R;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class BadgeAdapter extends BaseAdapter {
-    Context mContext;
-    LayoutInflater mLayoutInflater = null;
-    ArrayList<BadgeData> badge;
+public class BadgeAdapter extends RecyclerView.Adapter<BadgeAdapter.BadgeViewHolder> {
 
-    public BadgeAdapter(Context context, ArrayList<BadgeData> data) {
-        mContext = context;
-        badge = data;
-        mLayoutInflater = LayoutInflater.from(mContext);
+    private ArrayList<Badge> arrayList;
+    private Context context;
+
+    public BadgeAdapter(ArrayList<Badge> arrayList, Context context) {
+        this.arrayList = arrayList;
+        this.context = context;
     }
 
-
+    @NonNull
     @Override
-    public int getCount() {
-        return badge.size();
-    }
+    public BadgeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-    @Override
-    public Object getItem(int position)
-    {
-        return badge.get(position);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.mybadge_item, parent, false);
+        BadgeViewHolder holder = new BadgeViewHolder(view);
+
+        return holder;
     }
 
     @Override
-    public long getItemId(int position) {
-        return position;
+    public void onBindViewHolder(@NonNull BadgeViewHolder holder, int position) {
+           Glide.with(holder.itemView)
+                    .load(arrayList.get(position).getBadgeImage())
+                    .override(60, 60)
+                    .into(holder.badge_image);
+            holder.badge_name.setText(arrayList.get(position).getBadgeName());
+            holder.get_Date.setText(arrayList.get(position).getGetDate());
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View view = mLayoutInflater.inflate(R.layout.mybadge_item, null);
-
-        ImageView imageView = (ImageView)view.findViewById(R.id.badge_image);
-        TextView badge_name = (TextView)view.findViewById(R.id.badge_name);
-        TextView get_Date = (TextView)view.findViewById(R.id.get_Date);
-
-        imageView.setImageResource(badge.get(position).getBadge_image());
-        badge_name.setText(badge.get(position).getBadge_name());
-        get_Date.setText(badge.get(position).getDate());
-
-        return view;
+    public int getItemCount() {
+        return (arrayList != null ? arrayList.size():0) ;
     }
 
+    public class BadgeViewHolder extends RecyclerView.ViewHolder {
+
+
+       ImageView badge_image;
+        TextView badge_name;
+        TextView get_Date;
+
+        public BadgeViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+           this.badge_image = itemView.findViewById(R.id.badge_image);
+            this.badge_name = itemView.findViewById(R.id.badge_name);
+            this.get_Date = itemView.findViewById(R.id.get_Date);
+        }
+    }
 }
